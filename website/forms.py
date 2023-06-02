246a0da4b734ce9b2ca004e2f025755341e2be8a
Dbarbanakov <dbarbanakov@gmail.com>
2023-06-02 16:16:34 +0300

    done

--- website/forms.py
+++ website/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import Record
 
 
 class SignUpForm(UserCreationForm):
@@ -10,15 +11,19 @@ class SignUpForm(UserCreationForm):
             attrs={"class": "form-control", "placeholder": "Email Address"}
         ),
     )
-    first_name = forms.TextInput(
+    first_name = forms.CharField(
         label="",
         max_length=32,
-        attrs={"class": "form-control", "placeholder": "First Name"},
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "First Name"},
+        ),
     )
-    last_name = forms.TextInput(
+    last_name = forms.CharField(
         label="",
         max_length=32,
-        attrs={"class": "form-control", "placeholder": "Last Name"},
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Last Name"},
+        ),
     )
 
     class Meta(User):
@@ -55,3 +60,73 @@ class SignUpForm(UserCreationForm):
             self.fields[
                 "password2"
             ].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'
+
+
+class AddRecordForm(forms.ModelForm):
+    first_name = forms.CharField(
+        required=True,
+        widget=forms.widgets.TextInput(
+            attrs={"placeholder": "First Name", "class": "form-control"}
+        ),
+        label="",
+    )
+
+    last_name = forms.CharField(
+        required=True,
+        widget=forms.widgets.TextInput(
+            attrs={"placeholder": "Last Name", "class": "form-control"}
+        ),
+        label="",
+    )
+
+    email = forms.CharField(
+        required=True,
+        widget=forms.widgets.TextInput(
+            attrs={"placeholder": "Email Address", "class": "form-control"}
+        ),
+        label="",
+    )
+
+    phone = forms.CharField(
+        required=True,
+        widget=forms.widgets.TextInput(
+            attrs={"placeholder": "Phone Number", "class": "form-control"}
+        ),
+        label="",
+    )
+
+    address = forms.CharField(
+        required=True,
+        widget=forms.widgets.TextInput(
+            attrs={"placeholder": "Address", "class": "form-control"}
+        ),
+        label="",
+    )
+
+    city = forms.CharField(
+        required=True,
+        widget=forms.widgets.TextInput(
+            attrs={"placeholder": "City", "class": "form-control"}
+        ),
+        label="",
+    )
+
+    state = forms.CharField(
+        required=True,
+        widget=forms.widgets.TextInput(
+            attrs={"placeholder": "State", "class": "form-control"}
+        ),
+        label="",
+    )
+
+    zip = forms.CharField(
+        required=True,
+        widget=forms.widgets.TextInput(
+            attrs={"placeholder": "ZIP", "class": "form-control"}
+        ),
+        label="",
+    )
+
+    class Meta:
+        model = Record
+        exclude = ("user",)
--- website/models.py
+++ website/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Record(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    email = models.CharField(max_length=32)
+    phone = models.CharField(max_length=32)
+    address = models.CharField(max_length=32)
+    city = models.CharField(max_length=32)
+    state = models.CharField(max_length=32)
+    zip = models.CharField(max_length=32)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
